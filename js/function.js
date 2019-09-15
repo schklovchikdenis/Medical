05196e627dbf5e6cbbc261e149a8e6534fa06db9
Денис <denis@iMac-Denis.local>
2019-09-15 22:45:38 +0500

    Мобильное меню

--- js/function.js
+++ js/function.js
@@ -5,4 +5,23 @@ $(document).ready(function() {
       autoplay: true,
       autoplaySpeed: 5000,
       });
+
+      var HeaderTop = $('#home').offset().top;
+
+      // При срабатывании события scroll(прокрутка страницы)
+      $(window).scroll(function () {
+            // Задаем условия
+            if ($(window).scrollTop() > HeaderTop) {
+                  // Если высота прокрутки больше чем значение в переменной добавляем класс stiky
+                  $('#home').addClass('stiky');
+            } else {
+                  // Если высота прокрутки меньше чем значение в переменной убираем класс stiky
+                  $('#home').removeClass('stiky');
+            }
+      });
+
+      $('.header__mobileToggle').on('click', function (event) {
+            event.preventDefault();
+            $('.header-nav').toggleClass('open');
+      });
 });
